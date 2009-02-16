96d97d0c960c461904ebbbd28be9913d91b29031
vlad.andersen <vlad.andersen@43731c40-cfac-11dd-a2b5-e5f827957e07>
2009-02-16 11:52:29 +0000

    Fixing revert command
    git-svn-id: http://mygrate.googlecode.com/svn/trunk@16 43731c40-cfac-11dd-a2b5-e5f827957e07

--- cmds/revert.py
+++ cmds/revert.py
@@ -3,13 +3,19 @@
 import db.dump
 from db.table import Database
 import cmds.init
+import repo.revision
+import sys
 
 def run (args):
   """Reverts repository to its last commited state"""
   cmds.init.require_init()
   if not len(args):
-    print "Comparing current version to latest commited revision."
+    print "Comparing current database state to latest commited revision."
     src = Database().parseString(repo.revision.latest())
     dest = Database().parseString(db.dump.dump())
     revertSQL = str(src - dest)
+    if revertSQL == '':
+      print "Nothing to revert since revision #%s." % repo.revision.latest_number()
+      sys.exit()
+    print """Reverting to last committed revision: #%s.""" % repo.revision.latest_number()
     db.dump.load(revertSQL)
