4f4e60e878d91d1ed9832988074cb579b4b9b748
vlad.andersen <vlad.andersen@43731c40-cfac-11dd-a2b5-e5f827957e07>
2009-03-01 10:48:25 +0000

    --abandon option in mygrate update
    git-svn-id: http://mygrate.googlecode.com/svn/trunk@21 43731c40-cfac-11dd-a2b5-e5f827957e07

--- cmds/update.py
+++ cmds/update.py
@@ -6,17 +6,19 @@ import repo.migration
 import cmds.init
 import db.dump
 from db import MigrationFailedError
+from optparse import OptionParser
 
 def run (args = []):
   """Updates database to given revision"""
   cmds.init.require_init()
+  (options, args) = optargs (args)
   try:
     revision = int(args[0])
   except IndexError:
     revision = repo.migration.latest_number()
 
   current = repo.revision.current()
-  if current == revision:
+  if current == revision and not options.abandon_current:
     print """Nothing to update."""
     return
 
@@ -39,7 +41,6 @@ def run (args = []):
         repo.revision.set_current (current)
       except MigrationFailedError:
         break
-    print """Updated to revision #%s.""" % repo.revision.current()
     
   else:
     # Upgrading
@@ -53,8 +54,17 @@ def run (args = []):
         repo.revision.set_current (current)
       except MigrationFailedError:
         break
-    print """Updated to revision #%s.""" % repo.revision.current()
 
-  if outstanding_changes:
+  print """Updated to revision #%s.""" % repo.revision.current()
+
+  if outstanding_changes and not options.abandon_current:
     print """Reapplying outstanding changes."""
     db.dump.load (apply_after_update)
+
+def optargs(args):
+  """Parses options for current command."""
+  parser = OptionParser()
+  parser.add_option("", "--abandon", dest="abandon_current", default=False, action="store_true",
+                  help="Abandon outstanding changes when updating to migration")
+  (options, args) = parser.parse_args(args)
+  return (options, args)
