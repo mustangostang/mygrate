33529d1470da5ff517e82e97afabb1c41147723b
vlad.andersen <vlad.andersen@43731c40-cfac-11dd-a2b5-e5f827957e07>
2009-06-13 17:53:30 +0000

    Import functionality.
    git-svn-id: http://mygrate.googlecode.com/svn/trunk@31 43731c40-cfac-11dd-a2b5-e5f827957e07

---
+++ cmds/import.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import db.dump
+import cmds.init
+import db
+
+def run (args):
+  """Imports SQL file to the database"""
+  cmds.init.require_init()
+  if not len(args):
+    print "Specify an SQL file to run."
+  file = args[0]
+  try:
+    sql = open(file, 'rb').read()
+  except IOError:
+    print "Can't find file: %s" % file
+    return
+  try:
+    db.dump.load(sql)
+    print """SQL file imported. You can `commit` or `revert`."""
+  except db.SQLLoadError, e:
+    print "Can't import file: %s." % file
+    print e
+    
+    
