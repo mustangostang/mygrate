59de2e3185864a28240d039ba3468a313ce62f3c
vlad.andersen <vlad.andersen@43731c40-cfac-11dd-a2b5-e5f827957e07>
2010-01-31 20:56:35 +0000

    Debug mode for revert
    git-svn-id: http://mygrate.googlecode.com/svn/trunk@46 43731c40-cfac-11dd-a2b5-e5f827957e07

--- cmds/revert.py
+++ cmds/revert.py
@@ -5,10 +5,12 @@ from db.table import Database
 import cmds.init
 import repo.revision
 import sys
+from optparse import OptionParser
 
 def run (args):
   """Reverts repository to its last commited state"""
   cmds.init.require_init()
+  (options, args) = optargs (args)
   if not len(args):
     print "Comparing current database state to latest commited revision."
     src = Database().parseString(repo.revision.latest())
@@ -18,4 +20,17 @@ def run (args):
       print "Nothing to revert since revision #%s." % repo.revision.latest_number()
       sys.exit()
     print """Reverting to last committed revision: #%s.""" % repo.revision.latest_number()
-    db.dump.load(revertSQL)
+    if not options.debug:
+      db.dump.load(revertSQL)
+      return
+    for line in revertSQL.split("\n"):
+      print line.strip() + "\n"
+      print db.dump.load (line.strip())
+
+def optargs(args):
+  """Parses options for current command."""
+  parser = OptionParser()
+  parser.add_option("-d", "--debug", dest="debug", action="store_true", default = False,
+                  help="Run revert line-by-line with debug info")
+  (options, args) = parser.parse_args(args)
+  return (options, args)
